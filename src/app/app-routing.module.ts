c186006fd0b09144b46a743b1d7fb687e322ec9b
unknown <srinirajaram@gmail.com>
2020-10-14 21:28:46 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; 
+import { AboutComponent } from './about/about.component'; 
+import { ServiceComponent } from './service/service.component';
+import {ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }, // redirect to `first-component`
+  {path:"home", component:HomeComponent,}, 
+  {path:"about", component:AboutComponent}, 
+  {path:"service", component:ServiceComponent}, 
+  {path:"contact", component:ContactComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
